6064d80793b3a41e16141e21de572ce7374ee38d
borgified <borgified@gmail.com>
2019-11-03 01:26:23 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,11 @@
 
 apt-get -y update
 apt-get -y install slim git i3
+
+# minimal login manager
 git clone https://github.com/borgified/slim-theme-minimal /usr/share/slim/themes/slim-theme-minimal
 sed -i 's/current_theme\s+debian-softwaves/current_theme slim-theme-minimal/' /etc/slim.conf
+
+# no splash screen
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
+update-grub2
