7dba1b4777f1a7d7386f604869c2c2d8b9abdcd3
borgified <borgified@gmail.com>
2019-11-03 01:34:20 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ apt-get -y install slim git i3
 
 # minimal login manager
 git clone https://github.com/borgified/slim-theme-minimal /usr/share/slim/themes/slim-theme-minimal
-sed -i 's/current_theme\s+debian-softwaves/current_theme slim-theme-minimal/' /etc/slim.conf
+sed -i 's/current_theme.*debian-softwaves/current_theme slim-theme-minimal/' /etc/slim.conf
 
 # no splash screen
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
-update-grub2
+#sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
+#update-grub2
+# cant update-grub2 with 19.10 with zfs on root
